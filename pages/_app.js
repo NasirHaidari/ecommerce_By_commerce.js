50c056cdd362a3d5091474042b5bdc9565eb8a6d
NasirHaidari <nasir.haidri@htomail.com>
2021-05-29 00:50:32 +0200

    the store in done

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
 import '../styles/globals.css'
+import { StoreProvider } from '../components/Store'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+import Router from 'next/router'
+
+Router.events.on('routerChangeStart', () => NProgress.start())
+Router.events.on('routerChangeComplete', () => NProgress.done())
+Router.events.on('routerChangeError', () => NProgress.done())
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <StoreProvider>
+      <Component {...pageProps} />
+    </StoreProvider>
+  )
 }
 
 export default MyApp
+
+MyApp.getInitialProps = async () => {
+  return {
+    pageProps: {
+      commercePublicKey: process.env.COMMERCE_PUBLIC_KEY,
+    },
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,47 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import getCommerce from '../utils/commerce'
 
-export default function Home() {
+export default function Home({ products }) {
+  console.log(products)
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>MyFakeShop</title>
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        {products.map((product) => {
+          return (
+            <div key={product.id}>
+              <img src={product.media.source} alt={product.name} />
+              <p>{product.name}</p>
+              <p>{product.price.formatted_with_symbol}</p>
+            </div>
+          )
+        })}
       </main>
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+          href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
+          target='_blank'
+          rel='noopener noreferrer'>
           Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          <img src='/vercel.svg' alt='Vercel Logo' className={styles.logo} />
         </a>
       </footer>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const commerce = getCommerce()
+  const { data: products } = await commerce.products.list()
+  return {
+    props: {
+      products,
+    },
+  }
 }
